7425827bdb0188d6d1d0e62a126ae12f4cfc4eb9
Ghulam Yaseen Attari <muhammadyaseen3294@gmail.com>
2026-09-21 19:46:51 +0500

    Register IPsec networking article

--- src/blog/index.ts
+++ src/blog/index.ts
@@ -1,8 +1,10 @@
 import reactEnterprise from "./posts/react-enterprise";
 import vpsServerSetup from "./posts/vps-server-setup";
 import mongodbVpsSetup from "./posts/mongodb-vps-setup";
+import ipsecVpsTunnel from "./posts/ipsec-vps-tunnel";
 
 export const blogs = [
+  ipsecVpsTunnel,
   vpsServerSetup,
   mongodbVpsSetup,
   reactEnterprise
