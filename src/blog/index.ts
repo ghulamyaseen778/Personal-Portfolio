c44586d3509e2be029406143fd37682ecde4916b
Ghulam Yaseen Attari <muhammadyaseen3294@gmail.com>
2026-09-21 20:17:38 +0500

    Register VPS load balancer article

--- src/blog/index.ts
+++ src/blog/index.ts
@@ -2,8 +2,10 @@ import reactEnterprise from "./posts/react-enterprise";
 import vpsServerSetup from "./posts/vps-server-setup";
 import mongodbVpsSetup from "./posts/mongodb-vps-setup";
 import ipsecVpsTunnel from "./posts/ipsec-vps-tunnel";
+import vpsLoadBalancer from "./posts/vps-load-balancer";
 
 export const blogs = [
+  vpsLoadBalancer,
   ipsecVpsTunnel,
   vpsServerSetup,
   mongodbVpsSetup,
